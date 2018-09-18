45eee336ab874c9150908ec3cf8b8af7e76b7de5
hanqizheng <hanqizheng598@gmail.com>
2018-09-18 17:30:14 +0800

    replace LastWord from js to py

---
+++ Easy/LastWord.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = s.split(' ')
+        result = []
+
+        if len(s) == 0:
+            return 0
+        
+        for i in range(len(res)):
+            if res[i] != '':
+                result.append(res[i])
+        length = result[len(result) - 1]
+        return len(length)
+
+
+test = Solution()
+s = 'asdasd asda sfaf adadsdasada'
+print test.lengthOfLastWord(s)
