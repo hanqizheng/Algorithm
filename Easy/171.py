d315f372f80b3b3f9e39d2109ce4261f0d501015
hanqizheng <hanqizheng598@gmail.com>
2018-11-20 18:01:46 +0800

    add 171

--- Easy/171.py
+++ Easy/171.py
@@ -5,6 +5,6 @@ class Solution(object):
         :rtype: int
         """
         num = 0
-        for i in range(len(s)-1,-1,-1):
+        for i in range(len(s) - 1, -1, -1):
             num += (ord(s[i]) - 64) * 26**(len(s)-i-1)
         return num
