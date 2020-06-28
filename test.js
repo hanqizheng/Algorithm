8775e681f4acf71a725571e4a9c6c7c3ef7376fc
hanqizheng <hanqizheng598@gmail.com>
2020-06-28 16:39:40 +0800

    finish an interview question

--- test.js
+++ test.js
@@ -1,31 +1,43 @@
-// function test(a) {
-//   return new Promise((resolve, reject) => {
-//     if (a === 1) {
-//       resolve(a)
-//     } else {
-//       throw new Error('error')
-//       reject('errrrr')
-//     }
-//   })
-// }
 
-// // test(1).then(val => {
-// //   console.log(val)
-// // })
 
-// test(2).then(val => {
-//   console.log(val)
-// })
-// console.log('12344')
+function compare(a, b, c) {
+  // 将目标转换成数组处理
+  let strA = [...a]
+  let strB = [...b]
+  // 结果数组，最后变为字符串
+  let result = []
+  for(let i = 0; i < strA.length; i++) {
+    let j = 0
+    // 让i记录其实位置，pivot则用来移动比较目标字符串
+    let pivot = i
+    for (;j < strB.length; j++) {
+      if (strA[pivot] !== strB[j]) break
+      pivot++
+    }
+    if (j === strB.length) {
+      for(let k = i; k < strB.length + i; k++) {
+        // 都标记为0是便于以后替换，0是要舍弃的数组元素
+        strA[k] = 0
+      }
+      // 标记为1的地方，是要替换成目标字符串的元素
+      strA[i] = 1
+    }
+  }
+  // 舍弃0
+  result = [...strA].filter(item => item !== 0)
+  // 替换
+  result = result.map(item => {
+    if (item === 1) {
+      return [c]
+    } else {
+      return item
+    }
+  })
+  return result.join('')
+}
 
-const a = new Promise((resolve, reject) => {
-  resolve(111)
-}).then(val => {
-  throw new Error('error1')
-  return val
-}).then(val => {
-  console.log(val)
-  throw new Error('error2')
-}).catch(err => {
-  console.log(err)
-})
+compare(
+  'aabcdecedef',
+  'e',
+  'g'
+)
